db3202660e439991f8e338a47031e72b22b88561
otgonbayarzz <otgonbayar.eb@gmail.com>
2021-04-12 22:03:30 +0900

    cl fix

--- src/paymentItemDetail.js
+++ src/paymentItemDetail.js
@@ -2,6 +2,7 @@ const md5 = require("md5");
 const tool = require("./lib/tool");
 const sql = require('mssql')
 const moment = require('moment'); // require
+require('dotenv').config()
 
 function setup({ app, instance }) {
     app.get("/api/paymentItemDetail", async (req, res) => {
@@ -12,7 +13,7 @@ function setup({ app, instance }) {
         };
         try {
             // make sure that any items are correctly URL encoded in the connection string
-            await sql.connect('mssql://sa:123qweASD@localhost/demo')
+            await sql.connect(`mssql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_MAIN}`)
             const result = await sql.query`select * from SalesItemDetail `
             resp.data = result;
         } catch (err) {
@@ -67,7 +68,7 @@ function setup({ app, instance }) {
             }
 
             try {
-                await sql.connect('mssql://sa:123qweASD@localhost/demo')
+                await sql.connect(`mssql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_MAIN}`)
                 const result = await sql.query(qry.slice(0, -1));
                 resp.message = `Affected Detail rows count : ${result.rowsAffected[0]}`
             } catch (err) {
